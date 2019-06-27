dfc611dede54690b33beb4b7477bf2b3abbdd678
Nikita G <nikita.g@smartum.pro>
2019-06-27 10:24:05 +0300

    Source code added

---
+++ 0_simple_onboarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  0_simple_onboarding
+//
+//  Created by Nikita on 6/26/19.
+//  Copyright © 2019 gavrilovi.ch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
